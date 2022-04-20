9de00280201660373dd31501d64f0aeeaf50b202
JacobKelner <101436985+JacobKelner@users.noreply.github.com>
2022-04-19 21:50:58 -0700

    Basic Submission

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,23 @@
+var A='This is a string';
+window.alert(A)
+
+
+
+document.write("Lisa told Bart, \"Knock it off Bart! Or I'll tell dad!\" <br> \"Eat my shorts!\" Bart responded.");
+    document.write("<br>");
+document.write("\"Be who you are and say what you feel,"
++ "because those who mind don\'t matter and those who matter don\'t mind.\""
++"- Dr. Seuss");
+
+    document.write("<br>");
+
+var B="concatenated" + "String"
+document.write(B);//This prints variable B
+    document.write("<br>");
+var Family="The Smiths", Dad="Will", Mom="Jada", Daughter="Willow", Son="Jaden";
+document.write(Mom);//This prints variable Mom(Jada)
+
+document.write(" 3+3");
+
+var str=new String("Demo text");
+document.write(str.fontcolor("blue"));
