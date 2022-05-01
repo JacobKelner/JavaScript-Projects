dfd05bb2861afa1589b98b65720bbe63b5fa3527
JacobKelner <101436985+JacobKelner@users.noreply.github.com>
2022-04-30 22:02:40 -0700

    comment upload

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,33 +1,33 @@
-//LOOP FUNCTION//
+//WHILE LOOP FUNCTION//
 function Call_Loop() {
-    var Digit = "";
-    var X = 1;
-    while (X < 11) {
-        Digit += "<br>" + X;
-        X++;
+    var Digit = "";//"Digit" becomes variable
+    var X = 1;//'X' is variable with value of 1
+    while (X < 11) {//This states that while 'X' is less than 11 do the next step. if not, stop.
+        Digit += "<br>" + X;//Variable 'Digit' adds 'X' and adds a <br>
+        X++;//'X' increases its value by 1 each loop
     }
-    document.getElementById("Loop").innerHTML = Digit;
+    document.getElementById("Loop").innerHTML = Digit;// outputs the value of 'Digit' when loop is exited
 }
 
 //FOR LOOP FUNCTION//
 var Player = ["Lillard", "LeBron", "Antentokounmpo", "Embid", "Jokic", "Curry", "Morant", "Durant"];
 var Content = "";
-var Y;
+var Y;//'Y' represents different spots inside our array(We have 8. 0-7)
 function for_Loop() {
-    for (Y = 0; Y < Player.length; Y++) {
-        Content += Player[Y] + "<br>";
+    for (Y = 0; Y < Player.length; Y++) {//'Y' increments by one each loop, while it is less than variable length
+        Content += Player[Y] + "<br>";//break after each line/input
     }
-    document.getElementById("List_of_Players").innerHTML = Content;
+    document.getElementById("List_of_Players").innerHTML = Content;//output variable content
 }
 
 //ARRAY WITHIN A FUNCTION//
-function player_scoringattribute() {
+function player_scoringattribute() {//Function to connect to on HTML
     var wayof_Scoring = [];
-    wayof_Scoring[0]="Free Throw";
+    wayof_Scoring[0]="Free Throw";//Each 'wayof_Scoring' is a variable, with a different value on each one
     wayof_Scoring[1]="Layup";
     wayof_Scoring[2]="Three Pointer";
     wayof_Scoring[3]="Dunk";
-    document.getElementById("Score").innerHTML = "Damian Lillard wins the game, with a " + wayof_Scoring[2] + " at the end of regulation.";
+    document.getElementById("Score").innerHTML = "Damian Lillard wins the game, with a " + wayof_Scoring[2] + " at the end of regulation.";//Whatever number is inside the brackets '[]' determines what will be filled into the variables spot, being that there are multiple with there own value
 }
 
 //CONST KEYWORD FUNCTION//
@@ -40,13 +40,13 @@ function constant_function() {
 
 //LET KEYWORD//
 function let_Function() {
-    let X = 57
-    document.getElementById("let").innerHTML = X
+    let X = 57//let is a local variable
+    document.getElementById("let").innerHTML = X//display 'X'
 }
 
 //RETURN STATEMENT//
 function return_Function() {
-    document.getElementById("return").innerHTML = Math.PI;
+    document.getElementById("return").innerHTML = Math.PI;//Returns the value of PI
 }
 
 //OBJECT FUNCTION W PROPERTIES AND A METHOD//
@@ -66,8 +66,8 @@ document.getElementById("whip").innerHTML = whip.description();
 //BREAK STATEMENT (LOOP)//
 let text = "";
 for (let i = 0; i < 11; i++) {
-    if (i ==10) break;
-    if (i ==4) continue;
+    if (i ==10) break;//once i equals ten, loop breaks
+    if (i ==4) continue;//once i is eual to four, "jump" over it
     text += i + "<br>";
 }
-document.getElementById("txt").innerHTML = text;
+document.getElementById("txt").innerHTML = text;//output variable 'text'
